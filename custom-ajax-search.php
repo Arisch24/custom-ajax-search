dc190d74273caba540410e85ca39bf2598d76dc0
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-01-17 11:41:08 +0800

    Fix: only enqueue in front-end

--- custom-ajax-search.php
+++ custom-ajax-search.php
@@ -22,17 +22,19 @@ class CustomAjaxSearch
     // Enqueue scripts and styles
     function custom_ajax_search_init()
     {
-        // styles
-        wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-        wp_enqueue_style('custom-ajax-search', plugin_dir_url(__FILE__) . 'build/style-index.css', array(), '1.0', 'all');
-        wp_enqueue_style('custom-ajax-search', plugin_dir_url(__FILE__) . 'build/index.css', array(), '1.0', 'all');
-        // scripts
-        wp_enqueue_script('jquery');
-        wp_enqueue_script('custom-ajax-search', plugin_dir_url(__FILE__) . 'build/index.js', array('jquery'), '1.0', true);
-        wp_localize_script('custom-ajax-search', 'searchData', array(
-            'root_url' => esc_url_raw(get_site_url()),
-            'nonce' => wp_create_nonce('wp_rest'),
-        ));
+        if (!is_admin()) {
+            // styles
+            wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+            wp_enqueue_style('custom-ajax-search', plugin_dir_url(__FILE__) . 'build/style-index.css', array(), '1.0', 'all');
+            wp_enqueue_style('custom-ajax-search', plugin_dir_url(__FILE__) . 'build/index.css', array(), '1.0', 'all');
+            // scripts
+            wp_enqueue_script('jquery');
+            wp_enqueue_script('custom-ajax-search', plugin_dir_url(__FILE__) . 'build/index.js', array('jquery'), '1.0', true);
+            wp_localize_script('custom-ajax-search', 'searchData', array(
+                'root_url' => esc_url_raw(get_site_url()),
+                'nonce' => wp_create_nonce('wp_rest'),
+            ));
+        }
     }
 }
 
